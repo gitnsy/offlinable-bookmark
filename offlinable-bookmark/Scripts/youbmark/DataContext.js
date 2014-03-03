b68be04a0e0820ef0b966a2b2e5af2be2a4990fb
gitnsy <jump24d@gmai.com>
2014-03-03 22:29:07 +0900

    dum

--- offlinable-bookmark/Scripts/youbmark/DataContext.js
+++ offlinable-bookmark/Scripts/youbmark/DataContext.js
@@ -1,24 +1,39 @@
 ﻿var youbmark = youbmark || {};
 (function () {
-    //DataLayer(DataContext)
     dc = function () { };
     dc.prototype = {
         save: function (bookmark) {
+            /// <summary>save bookmark to Persistent storage</summary>
+            /// <param name="json" type="String">bookmark object</param>
+
             localStorage.setItem(bookmark.url, bookmark.toJson());
         }
+
         , get: function (url) {
+            /// <summary>get bookmark</summary>
+            /// <param name="json" type="String">bookmark url</param>
+            /// <returns type="Bookmark">Deserialized Bookmark</returns>
+
             var a = localStorage.getItem(url);
             return a && youbmark.Bookmark.parse(a);
         }
+
         , getAll: function () {
+            /// <summary>get all bookmarks sorted by date</summary>
+            /// <returns type="Bookmark">all bookmarks sorted by date</returns>
+
             var result = [];
             for (var i = 0; i < localStorage.length; i++) {
                 result.push(youbmark.Bookmark.parse(localStorage.getItem(localStorage.key(i))));
             }
-            return result.sort(function(a,b){return a.createDate > b.createDate;});
+            return result.sort(function(a,b){return a.createDate - b.createDate;});
         }
-        , remove: function (url) { localStorage.removeItem(url); }
-        , clear: function () { localStorage.clear(); }
+        , remove: function (url) {
+            /// <summary>remove bookmark from Persistent storage</summary>
+            /// <param name="json" type="String">bookmark url</param>
+
+            localStorage.removeItem(url);
+        }
     };
 
     youbmark.DataContext = dc;
